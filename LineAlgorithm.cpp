b57305c6d68f7a97037a3f6624ddcde5b6722c37
Kevmobeans <kevincye1996@gmail.com>
2016-01-12 23:16:26 -0800

    Initial commit

---
+++ LineAlgorithm.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @author The CS2 TA Team <<cs2-tas@ugcs.caltech.edu>>
+ * @version 1.0
+ * @date 2013-2014
+ * @copyright This code is in the public domain.
+ *
+ * @brief Student implementation of line algorithm.
+ *
+ */
+#include "LineAlgorithm.h"
+
+/**
+ * TODO: Implement a line algorithm of your choice here.
+ */
+vector<Tuple> line(Tuple p1, Tuple p2)
+{
+    vector<Tuple> v;
+    return v;
+}
